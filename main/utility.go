03912b15fe203c9df444dc90f3fd1e3318c80974
Nicolas Barzotti <nicolasbarzotti@gmail.com>
2023-11-28 17:30:36 +0100

    Finished roughly translating the code, now performing sanity checks on each function and method

--- main/utility.go
+++ main/utility.go
@@ -8,10 +8,124 @@ func CreateMatrix[F float64 | float32](size int) [][]F {
 	return res
 }
 
-func OperateOnEachCell[F float64 | float32](matrix *[][]F, doOperation func(F)) {
+func OperateOnEachCell[F float64 | float32](matrix *[][]F, doOperation func(cell *F)) {
 	for _, fs := range *matrix {
 		for i := range fs {
-			doOperation(fs[i])
+			doOperation(&fs[i])
 		}
 	}
+}
+
+func OperateOnCellsWithIndex[F float64 | float32](matrix *[][]F, doOperation func(cell *F, i int, j int)) {
+	for i, fs := range *matrix {
+		for j := range fs {
+			doOperation(&fs[j], i, j)
+		}
+	}
+}
+
+func AddMatrixByScalar[F float64 | float32](matrix [][]F, scalar F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell += scalar
+	})
+	return &res
+}
+
+func AddPMatrixByScalar[F float64 | float32](matrix *[][]F, scalar F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell += scalar
+	})
+}
+
+func AddMatrices[F float64 | float32](matrix [][]F, matrix2 [][]F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell += matrix2[i][j]
+	})
+	return &res
+}
+
+func AddPMatrices[F float64 | float32](matrix *[][]F, matrix2 [][]F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell += matrix2[i][j]
+	})
+}
+
+func SubtractMatrices[F float64 | float32](matrix [][]F, matrix2 [][]F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell -= matrix2[i][j]
+	})
+	return &res
+}
+
+func SubtractPMatrices[F float64 | float32](matrix *[][]F, matrix2 [][]F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell -= matrix2[i][j]
+	})
+}
+
+func MultiplyMatrixByScalar[F float64 | float32](matrix [][]F, scalar F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell *= scalar
+	})
+	return &res
+}
+
+func MultiplyPMatrixByScalar[F float64 | float32](matrix *[][]F, scalar F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell *= scalar
+	})
+}
+
+func MultiplyMatrices[F float64 | float32](matrix [][]F, matrix2 [][]F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell *= matrix2[i][j]
+	})
+	return &res
+}
+
+func MultiplyPMatrices[F float64 | float32](matrix *[][]F, matrix2 [][]F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell *= matrix2[i][j]
+	})
+}
+
+func DivideMatrixByScalar[F float64 | float32](matrix [][]F, scalar F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell /= scalar
+	})
+	return &res
+}
+
+func DividePMatrixByScalar[F float64 | float32](matrix *[][]F, scalar F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell /= scalar
+	})
+}
+
+func DivideMatrices[F float64 | float32](matrix [][]F, matrix2 [][]F) *[][]F {
+	res := CreateMatrix[F](len(matrix))
+	OperateOnCellsWithIndex[F](&res, func(cell *F, i int, j int) {
+		*cell /= matrix2[i][j]
+	})
+	return &res
+}
+
+func DividePMatrices[F float64 | float32](matrix *[][]F, matrix2 [][]F) {
+	OperateOnCellsWithIndex[F](matrix, func(cell *F, i int, j int) {
+		*cell /= matrix2[i][j]
+	})
+}
+
+func SumWholeArray[F float64 | float32](matrix [][]F) F {
+	var res F
+	OperateOnEachCell(&matrix, func(f *F) {
+		res += *f
+	})
+	return res
 }
--- main/xy_model.go
+++ main/xy_model.go
@@ -55,8 +55,17 @@ func (m *Model) printValues() {
 	println("muB: ", m.muB)
 	println("Temperature: ", m.temperature)
 	println("MagField: ", m.magField)
-	OperateOnEachCell(&m.lattice, func(f float64) {
+	OperateOnEachCell(&m.lattice, func(f *float64) {
 		print(f)
+	})
+}
+
+func (m *Model) printLattice() {
+	OperateOnCellsWithIndex(&m.lattice, func(cell *float64, i int, j int) {
+		print(*cell)
+		if j == len(m.lattice[i])-1 {
+			println()
+		}
 	})
 }
 
@@ -65,26 +74,23 @@ func (m *Model) assign(value float64) error {
 	if 0 < value && value < math.Pi*2 {
 		err = errors.New("given value is out of bounds")
 	}
-	OperateOnEachCell[float64](&m.lattice, func(f float64) {
-		f = value
+	OperateOnEachCell[float64](&m.lattice, func(f *float64) {
+		*f = value
 	})
 
 	return err
 }
 func (m *Model) randomize() {
-	OperateOnEachCell[float64](&m.lattice, func(f float64) {
-		f = rand.Float64() * 2 * math.Pi
+	OperateOnEachCell[float64](&m.lattice, func(f *float64) {
+		*f = rand.Float64() * 2 * math.Pi
 	})
 }
 
 func (m *Model) energy() [][]float64 {
 	var res = CreateMatrix[float64](m.size)
-
-	for i := 0; i < m.size; i++ {
-		for j := 0; j < m.size; j++ {
-			res[i][j] = -m.coupling*(math.Cos(m.lattice[i][j]-m.lattice[i][(j+1)%m.size])) + math.Cos(m.lattice[i][j]-m.lattice[(i+1)%m.size][j]) - m.muB*m.magField*math.Cos(m.lattice[i][j])
-		}
-	}
+	OperateOnCellsWithIndex[float64](&res, func(cell *float64, i int, j int) {
+		*cell = -m.coupling*(math.Cos(m.lattice[i][j]-m.lattice[i][(j+1)%m.size])) + math.Cos(m.lattice[i][j]-m.lattice[(i+1)%m.size][j]) - m.muB*m.magField*math.Cos(m.lattice[i][j])
+	})
 	return res
 }
 
@@ -92,32 +98,99 @@ func (m *Model) energy() [][]float64 {
 func (m *Model) deltaEnergy(newAngles [][]float64) [][]float64 {
 	res := CreateMatrix[float64](m.size)
 	rows, cols := len(m.lattice), len(m.lattice[0])
-	dE := 0.0
 
-	for i := 0; i < rows; i++ {
-		for j := 0; j < cols; j++ {
-			dE += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[(i+1)%rows][j]) - math.Cos(m.lattice[i][j]-m.lattice[(i+1)%rows][j]))
-			dE += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[i][(j+1)%cols]) - math.Cos(m.lattice[i][j]-m.lattice[i][(j+1)%cols]))
-			dE += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[(i-1+rows)%rows][j]) - math.Cos(m.lattice[i][j]-m.lattice[(i-1+rows)%rows][j]))
-			dE += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[i][(j-1+cols)%cols]) - math.Cos(m.lattice[i][j]-m.lattice[i][(j-1+cols)%cols]))
-			dE += -m.muB * m.magField * (math.Cos(newAngles[i][j]) - math.Cos(m.lattice[i][j]))
-		}
-	}
+	OperateOnCellsWithIndex(&res, func(cell *float64, i int, j int) {
+		*cell += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[(i+1)%rows][j]) - math.Cos(m.lattice[i][j]-m.lattice[(i+1)%rows][j]))
+		*cell += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[i][(j+1)%cols]) - math.Cos(m.lattice[i][j]-m.lattice[i][(j+1)%cols]))
+		*cell += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[(i-1+rows)%rows][j]) - math.Cos(m.lattice[i][j]-m.lattice[(i-1+rows)%rows][j]))
+		*cell += -m.coupling * (math.Cos(newAngles[i][j]-m.lattice[i][(j-1+cols)%cols]) - math.Cos(m.lattice[i][j]-m.lattice[i][(j-1+cols)%cols]))
+		*cell += -m.muB * m.magField * (math.Cos(newAngles[i][j]) - math.Cos(m.lattice[i][j]))
+	})
 	return res
 }
 
 func (m *Model) magnetization() float64 {
 	var magX float64 = 0
 	var magY float64 = 0
-	OperateOnEachCell[float64](&m.lattice, func(f float64) {
-		magX += math.Cos(f)
-		magY += math.Sin(f)
+	OperateOnEachCell(&m.lattice, func(f *float64) {
+		magX += math.Cos(*f)
+		magY += math.Sin(*f)
 	})
 	return math.Sqrt(math.Pow(magX, 2) + math.Pow(magY, 2))
 }
 
+func (m *Model) transitionProbability(newAngles [][]float64) [][]float64 {
+	res := CreateMatrix[float64](m.size)
+
+	deltaMatrix := m.deltaEnergy(newAngles)
+	OperateOnCellsWithIndex(&res, func(cell *float64, i int, j int) {
+		*cell = math.Exp(-1.0 / m.temperature * deltaMatrix[i][j])
+	})
+	return res
+}
+
+func (m *Model) evolve(therm int, fraction float64, meas int, drop int) {
+	var age = 0
+	var iMeas = 0
+	var newAngles = CreateMatrix[float64](m.size)
+	var prob = CreateMatrix[float64](m.size)
+	var trans = CreateMatrix[float64](m.size)
+	var en = CreateMatrix[float64](m.size)
+	var en2 = CreateMatrix[float64](m.size)
+	var magPerSite float64 = 0
+	var mag float64 = 0
+	var mag2 float64 = 0
+	for i := 0; i < therm+meas; i++ {
+		OperateOnEachCell(&newAngles, func(f *float64) {
+			*f = rand.NormFloat64()
+			*f *= 2 * math.Pi
+		})
+
+		prob = m.transitionProbability(newAngles)
+
+		OperateOnCellsWithIndex(&trans, func(cell *float64, i int, j int) {
+			transition := rand.Float64() < prob[i][j] && rand.Float64() < fraction
+			if transition {
+				m.lattice[i][j] = newAngles[i][j]
+			}
+		})
+
+		age++
+
+		if i > therm && i%drop == 0 {
+			tmpE := CreateMatrix[float64](m.size)
+			var tmpM float64
+			tmpE = m.energy()
+			tmpM = m.magnetization()
+
+			AddPMatrices(&en, tmpE)
+			MultiplyPMatrices(&tmpE, tmpE)
+			AddPMatrices(&en2, tmpE)
+			magPerSite += tmpM / math.Pow(float64(m.size), 2)
+			mag += tmpM
+			mag2 += math.Pow(tmpM, 2)
+			iMeas++
+		}
+	}
+
+	DividePMatrixByScalar(&en, float64(iMeas))
+	DividePMatrixByScalar(&en2, float64(iMeas))
+	magPerSite /= float64(iMeas)
+	mag /= float64(iMeas)
+	mag2 /= float64(iMeas)
+
+	energeticVariance := CreateMatrix[float64](m.size)
+	energeticVariance = *SubtractMatrices(en2, *MultiplyMatrices(en, en))
+
+	magneticVariance := mag2 - mag*mag
+
+	specificHeat := 1 / (math.Pow(float64(m.size), 2) * math.Pow(m.temperature, 2)) * SumWholeArray(energeticVariance)
+	magSusPerSite := 1 / (math.Pow(float64(m.size), 2) * m.temperature) * magneticVariance
+
+	println(specificHeat, magSusPerSite)
+}
 func xyModel() {
 	h := newModel()
-	h.printValues()
-
+	h.randomize()
+	h.printLattice()
 }
